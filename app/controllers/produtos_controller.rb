952f2f24dc24293152b5bedfd996a27fbdeccdc1
Draichi <lucasdraichi@gmail.com>
2017-10-19 22:25:25 -0200

    partials

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -3,4 +3,13 @@ class ProdutosController < ApplicationController
     @produtos_por_nome = Produto.order :nome
     @produtos_por_preco = Produto.order :preco
   end
+
+  def create
+    nome = params["nome"]
+    preco = params["preco"]
+    descricao = params["descricao"]
+    quantidade = params["quantidade"]
+    valores = params.require(:produto).permit :nome, :descricao, :quantidade, :preco
+    produto = Produto.create valores
+  end
 end
