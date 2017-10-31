4169a8a57c1e8a0efaff98225154f211ad659969
--global <lucasdraichi@gmail.com>
2017-10-31 11:30:25 -0200

    form with partial render

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,4 +1,7 @@
 class ProdutosController < ApplicationController
+
+  before_action :set_produto, only: [:edit, :update, :destroy]
+
   def index
     @produtos_por_nome = Produto.order :nome
     @produtos_por_preco = Produto.order :preco
@@ -6,32 +9,62 @@ class ProdutosController < ApplicationController
 
   def new
     @produto = Produto.new
-    @departamentos = Departamento.all
+    render_view :new
   end
 
   def create
-    nome = params["nome"]
-    preco = params["preco"]
-    descricao = params["descricao"]
-    quantidade = params["quantidade"]
-    valores = params.require(:produto).permit :nome, :descricao, :quantidade, :preco, :departamento_id
-    @produto = Produto.new valores
+    @produto = Produto.new produto_params
+    @departamentos = Departamento.all
     if @produto.save
       flash[:notice] = "Produto adicionado"
       redirect_to root_url
     else
-      render :new
+      render_view :new
     end
   end
 
   def destroy
-    id = params[:id]
-    Produto.destroy id
+    @produto.destroy
     redirect_to root_url
   end
 
   def busca
     @nome_buscado = params[:nome]
     @produtos = Produto.where "nome like ?", "%#{@nome_buscado}%"
+  end
+
+  def edit
+    id = params[:id]
+    @produto = Produto.find(id)
+    render_view :edit
+  end
+
+  def update
+    id = params[:id]
+    @produto = Produto.find(id)
+    @departamentos = Departamento.all
+    if @produto.update produto_params
+      flash[:notice] = "Atualizado"
+      redirect_to root_url
+    else
+      render_view :edit
+    end
+  end
+
+
+  private
+
+  def render_view(view)
+    @departamentos = Departamento.all
+    render view
+  end
+
+  def set_produto
+    id = params[:id]
+    @produto = Produto.all
+  end
+
+  def produto_params
+    params.require(:produto).permit :nome, :preco, :descricao, :quantidade, :departamento_id
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :departamentos
   root "produtos#index"
-  resources :produtos, only: [:new, :destroy, :create]
+  resources :produtos, only: [:new, :destroy, :create, :edit, :update]
   #rake routes no terminal. resoureces vai dar todos os paths para destroy, new, etc
   get "/produtos/busca" => "produtos#busca", as: :busca_produto
 end
